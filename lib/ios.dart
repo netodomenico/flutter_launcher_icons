5ce8a90207e8f828830357d9d007767d94d44b43
Domenico Neto <netodomenico@me.com>
2023-03-22 21:25:03 +0100

    Update ios.dart

--- lib/ios.dart
+++ lib/ios.dart
@@ -68,7 +68,7 @@ void createIcons(FlutterLauncherIconsConfig config, String? flavor) {
       saveNewIcons(template, image, flavor);
     }
     iconName = iosDefaultIconName;
-    changeIosLauncherIcon('Ciao-Icon-App', flavor);
+    changeIosLauncherIcon('"\$(ASSET_PREFIX)AppIcon"', flavor);
     modifyContentsFile(flavor);
   } else if (iosConfig is String) {
     // If the IOS configuration is a string then the user has specified a new icon to be created
